6f10ec0f942239e98e8f573aa8a1c6e55246a406
maru8605 <maru8605@hotmail.com>
2021-08-31 21:41:37 -0300

    Creando hook de jobs

---
+++ src/hooks/useFetchJobs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const useFetchJobs = (params, page) => {
+    return {
+        jobs: '',
+        error: false,
+        loading:false
+    }
+}
+
+export default useFetchJobs
